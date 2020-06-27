c81bff93768d931033cb9795d2f724b7be0be0ee
Krizza Bullecer <krizzabullecer@gmail.com>
2020-06-27 21:58:28 +0800

    add style for scroll to bottom

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -6,7 +6,7 @@ import Message from './Message/Message';
 import './Messages.css';
 
 const Messages = ({ messages, name }) => (
-  <ScrollToBottom>
+  <ScrollToBottom className='messages'>
     {messages.map((message, idx) => (
       <div key={idx}>
         <Message message={message} name={name} />
