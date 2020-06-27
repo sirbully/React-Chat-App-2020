f91ebc6d8210684373b1215639acfec73ac776c6
Krizza Bullecer <krizzabullecer@gmail.com>
2020-06-27 19:56:22 +0800

    complete messages

---
+++ client/src/components/Messages/Messages.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+import Message from './Message/Message';
+
+import './Messages.css';
+
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom>
+    {messages.map((message, idx) => (
+      <div key={idx}>
+        <Message message={(message, name)} />
+      </div>
+    ))}
+  </ScrollToBottom>
+);
+
+export default Messages;
