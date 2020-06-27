98c87dfdc721383a9b622d8a9f1adf2d59641613
Krizza Bullecer <krizzabullecer@gmail.com>
2020-06-27 19:44:52 +0800

    implement socketio

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+
+io.on('connect', socket => {
+  console.log('We have a new connection');
+
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `Welcome to ${user.room}, ${user.name}!`,
+    });
+    socket.broadcast.to(user.room).emit('message', {
+      user: 'admin',
+      text: `${user.name} has joined the chat`,
+    });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User left');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}...`));
