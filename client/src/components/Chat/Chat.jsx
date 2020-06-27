25e386ab2af118fc4272a37e84c17d8ad8b499f5
Krizza Bullecer <krizzabullecer@gmail.com>
2020-06-27 21:57:37 +0800

    integrate user online component

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -5,6 +5,7 @@ import io from 'socket.io-client';
 import InfoBar from '../InfoBar/InfoBar';
 import Messages from '../Messages/Messages';
 import Input from '../Input/Input';
+import UserContainer from '../UserContainer/UserContainer';
 
 import './Chat.css';
 
@@ -13,6 +14,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
@@ -41,6 +43,10 @@ const Chat = ({ location }) => {
     socket.on('message', message => {
       setMessages([...messages, message]);
     });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
   }, [messages]);
 
   const sendMessage = event => {
@@ -50,8 +56,6 @@ const Chat = ({ location }) => {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   };
-
-  console.log(message, messages);
 
   return (
     <div className='outerContainer'>
@@ -64,6 +68,7 @@ const Chat = ({ location }) => {
           sendMessage={sendMessage}
         />
       </div>
+      <UserContainer users={users} />
     </div>
   );
 };
